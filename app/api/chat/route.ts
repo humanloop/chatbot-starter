7076d69a7faddacd378d21a11a779b600592ae22
Jordan Burgess <jordan@humanloop.com>
2023-09-19 14:25:35 +0100

    Update packages

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,39 +1,21 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
-
-import { nanoid } from '@/lib/utils'
+import OpenAI from 'openai'
 
 export const runtime = 'edge'
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
 
-const openai = new OpenAIApi(configuration)
-
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
+  const { messages } = await req.json()
 
-  if (previewToken) {
-    configuration.apiKey = previewToken
-  }
-
-  const res = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
     stream: true
   })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-    }
-  })
-
+  const stream = OpenAIStream(response)
   return new StreamingTextResponse(stream)
 }
