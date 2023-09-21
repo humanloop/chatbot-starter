6f56e7fed7eaa81646ab2c760018c4c4e2fce6e4
Jordan Burgess <jordan@humanloop.com>
2023-09-21 15:04:41 +0100

    warn if api key not set

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,15 +4,21 @@ import { Humanloop } from 'humanloop'
 
 export const runtime = 'edge'
 
+const HUMANLOOP_API_KEY = process.env.HUMANLOOP_API_KEY
+
+if (!HUMANLOOP_API_KEY) {
+  throw new Error('HUMANLOOP_API_KEY is not set')
+}
+
 const humanloop = new Humanloop({
   useFetch: true, // useFetch must be "true" for humanloop to work in Next.js edge runtime,
   openaiApiKey: process.env.OPENAI_API_KEY,
-  apiKey: process.env.HUMANLOOP_API_KEY
+  apiKey: HUMANLOOP_API_KEY
 })
 
 export async function POST(req: Request) {
   const { messages } = await req.json()
- 
+
   const chatResponse = await humanloop.chatStream({
     project: 'sdk-example',
     messages,
